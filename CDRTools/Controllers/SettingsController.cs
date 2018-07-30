de87067d2352915737e20a8f6c6ee6f279ecce35
Ricardo <Ricardo@DESKTOP-RS8NOBT>
2018-07-30 17:48:22 -0500

    Add project files.

---
+++ CDRTools/Controllers/SettingsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CDRTools.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ConfigMenu()
+        {
+            return View("_ConfigMenu");
+        }
+        public ActionResult LeerArchivo()
+        {
+            return View("_LeerArchivo");
+        }
+    }
+}
